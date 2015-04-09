e1e82b74c8724f372531d1adaebd05447afa4773
Jeevan James <jjames@stonegatemtg.com>
2015-04-09 12:30:05 +0530

    Temporarily added the tsd definition files to source control until the network issue with tsd is resolved.

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="angularjs/angular-route.d.ts" />
+/// <reference path="bootstrap/bootstrap.d.ts" />
+/// <reference path="angularjs/angular-sanitize.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
