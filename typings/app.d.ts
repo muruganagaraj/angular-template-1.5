a101858fe7f96dea4a2911e4dc9090f4fb13a45c
Jeevan James <jjames@stonegatemtg.com>
2015-04-11 19:01:45 +0530

    Updated app.d.ts.

--- typings/app.d.ts
+++ typings/app.d.ts
@@ -1,6 +1,6 @@
 //{
 /// <reference path="../client/app/App.ts" />
-/// <reference path="../client/app/main/Item.ts" />
+/// <reference path="../client/app/main/item.ts" />
 /// <reference path="../client/app/main/main.controller.ts" />
 /// <reference path="../client/app/main/my.service.ts" />
 /// <reference path="../client/app/other/other.controller.ts" />
