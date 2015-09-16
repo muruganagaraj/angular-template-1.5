ab7c492bc4fb96149ae94cb4487c733828277c5e
Jeevan James <jeevanjj@yahoo.com>
2015-09-16 19:27:26 +0530

    Removing all existing code for new version.

--- typings/app.d.ts
+++
@@ -1,7 +0,0 @@
-//{
-/// <reference path="../client/app/App.ts" />
-/// <reference path="../client/app/main/item.ts" />
-/// <reference path="../client/app/main/main.controller.ts" />
-/// <reference path="../client/app/main/my.service.ts" />
-/// <reference path="../client/app/other/other.controller.ts" />
-//}
