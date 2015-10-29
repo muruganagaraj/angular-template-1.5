1f9061b2ee582f3b2be0ae9ca622669997bfa565
Jeevan James <jeevanjj@yahoo.com>
2015-10-29 19:26:10 +0530

    Fixed build issues.

--- typings/lib.d.ts
+++ typings/lib.d.ts
@@ -6,6 +6,5 @@
 /// <reference path="angularjs/angular-sanitize.d.ts" />
 /// <reference path="angular-ui-bootstrap/angular-ui-bootstrap.d.ts" />
 /// <reference path="angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="ui-grid/ui-grid.d.ts" />
 /// <reference path="moment/moment-node.d.ts" />
 /// <reference path="moment/moment.d.ts" />
